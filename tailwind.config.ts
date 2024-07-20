571170e02f549a8994c7fdca04da64cbaabe0090
Codesmaniac <joyajiboye2016@gmail.com>
2024-07-20 16:33:17 +0100

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors:{
+      dark: '#1b1b1b',
+      white: '#ffffff',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
